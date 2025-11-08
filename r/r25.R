0c40aa828f840fed0fd1cc81b1b15fc70e72eff4
djedjigair <djedjigairnatene1@gmail.com>
2025-11-08 11:37:26 -0500

    r25

---
+++ r/r25.R
@@ -0,0 +1,48 @@
+# file- rnov/8/2025
+mtcars
+#describe----
+str(mtcars)
+summary(mtcars)
+names(mtcars)
+dim(mtcars)
+?dim(mtcars)
+head(mtcars)
+head(mtcars, 10)
+tail(mtcars)
+is.na(mtcars)
+sum(is.na(mtcars))
+df= mtcars
+df
+
+#analysis----
+head(mtcars, 2)
+sapply(mtcars, mean)
+
+mean(mtcars$mpg)
+min(mtcars$mpg)
+max(mtcars$mpg)
+range(mtcars$mpg)
+sd(mtcars$mpg)
+quantile(mtcars$mpg)
+summary(mtcars)
+
+#filter----
+mtcars[0:4, c('mpg', 'gear')]
+sapply(mtcars[,c('mpg', 'gear')], max)
+mtcars[3:6, 1:5]
+mtcars[c(1,5,8), c(3,8,9)]
+head(mtcars)
+
+#summary----
+
+library(dplyr)
+
+mtcars%>% summary()
+mtcars %>% mutate(newMPG =mpg * 1.5)
+mtcars %>% select (mpg, wt) %>% head(6)
+df = mtcars
+df
+df <- df %>% mutate(newMPG = mpg * 1.5)
+df
+df %>% group_by(gear) %>% summarise( n = n())
+df %>% group_by(gear , cyl) %>% summarise( n = n())
